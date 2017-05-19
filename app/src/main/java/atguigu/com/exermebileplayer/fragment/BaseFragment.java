8116ec08aa88ca62df6025e8ff7237e5b7dd01a0
liyinqing <yinqing_521@126.com>
2017-05-19 21:19:00 +0800

    页底完成

---
+++ app/src/main/java/atguigu/com/exermebileplayer/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package atguigu.com.exermebileplayer.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 作者：李银庆 on 2017/5/19 21:18
+ */
+public class BaseFragment extends Fragment {
+
+}
